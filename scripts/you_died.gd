996c0776cab7a68bce685483abc254e989b8360d
Matyáš Trnka <hello@matrn.dev>
2026-01-29 23:10:36 +0100

    add main menu and ui improvements

--- scripts/you_died.gd
+++ scripts/you_died.gd
@@ -4,3 +4,6 @@ signal play_again
 
 func _on_play_again_button_pressed() -> void:
 	play_again.emit()
+
+func _on_main_menu_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
