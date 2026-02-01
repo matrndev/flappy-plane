39675af347729416e82d7187c70bbc2824ee998e
Matyáš Trnka <hello@matrn.dev>
2026-02-01 22:02:20 +0100

    show assets' licenses in credits menu

--- scripts/credits_menu.gd
+++ scripts/credits_menu.gd
@@ -6,8 +6,24 @@ func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 	
 
-
+func load_file(path: String) -> void:
+	var real_path = ProjectSettings.globalize_path(path)
+	var file = FileAccess.open(real_path, FileAccess.READ)
+	if file:
+		$TextEdit.text = file.get_as_text()
 
 func _on_license_button_1_pressed() -> void:
-	var path = ProjectSettings.globalize_path("res://assets/flappy-bird-assets-master/LICENSE")
-	OS.shell_open(path)
+	load_file("res://assets/flappy-bird-assets-master/LICENSE")
+
+
+func _on_license_button_2_pressed() -> void:
+	load_file("res://assets/Flappy Bird Assets/readme.txt")
+
+
+func _on_license_button_3_pressed() -> void:
+	load_file("res://assets/plane_pack/sky_background/info.TXT")
+
+
+
+func _on_license_button_4_pressed() -> void:
+	load_file("res://assets/audio/cash_attrib.txt")
